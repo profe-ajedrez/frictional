86d81fddd0f6297a2ea1731a8e17d0137e6e958c
areyes-bsale <areyes@bsale.io>
2024-06-23 18:32:42 -0400

    initial

--- visitor.go
+++
@@ -1,8 +0,0 @@
-package frictional
-
-// Visitor is an interface that defines a method for performing an operation
-// on a Frictional.
-// The Do method takes a Frictional as an argument and performs some operation on it.
-type Visitor interface {
-	Do(Frictional)
-}
